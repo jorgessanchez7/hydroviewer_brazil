d5a9c79c6530da2339c26a513a460c83a2746203
Jéssica Domingues Lamosa <jdlamosa@gmail.com>
2023-01-04 14:45:08 -0300

    Updating ottobacias name

--- tethysapp/hydroviewer_brazil/public/js/ottobaciasIndex.js
+++ tethysapp/hydroviewer_brazil/public/js/ottobaciasIndex.js
@@ -1,18 +1,18 @@
 let ottobacias_index = {
 	  'level_1': {
-			'name': 'Ottobacia N1',
+			'name': 'Ottobacia L1',
 			'geojsons': ['ottobacias_nivel1.json']
 	  },
 	  'level_2': {
-			'name': 'Ottobacia N2',
+			'name': 'Ottobacia L2',
 			'geojsons': ['ottobacias_nivel2.json']
 	  },
 	  'level_3': {
-			'name': 'Ottobacia N3',
+			'name': 'Ottobacia L3',
 			'geojsons': ['ottobacias_nivel3.json']
 	  },
 	  'level_4': {
-			'name': 'Ottobacia N4',
+			'name': 'Ottobacia L4',
 			'geojsons': ['ottobacias_nivel4.json']
 	  },
 };

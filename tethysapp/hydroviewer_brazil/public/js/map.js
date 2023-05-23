e7572d3d2bfc35945b3bf5ad8ece68313bf31c70
Jéssica Domingues Lamosa <jdlamosa@gmail.com>
2023-05-23 12:35:24 -0300

    Adding information modal about the streams

--- tethysapp/hydroviewer_brazil/public/js/map.js
+++ tethysapp/hydroviewer_brazil/public/js/map.js
@@ -645,10 +645,15 @@ const regionsStyle = () => {
     moveDown.classList.add('observed-layer-control', 'move-down');
     moveDown.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>';
 
+    const info = document.createElement('div');
+    info.classList.add('observed-layer-control', 'info');
+    info.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z"/></svg>'
+
     controls.append(moveUp);
     controls.append(moveDown);
     controls.append(toggle);
     controls.append(remove);
+    controls.append(info);
     wrapper.append(title);
     wrapper.append(controls);
 
@@ -668,6 +673,10 @@ const regionsStyle = () => {
     $(moveDown).click(() => {
         $(wrapper).next().insertBefore(wrapper);
         moveLayer(1);
+    });
+
+    $(info).click(() => {
+        $('#about-'+ title.textContent).modal('show');
     });
   
     $(toggle).click(() => {
